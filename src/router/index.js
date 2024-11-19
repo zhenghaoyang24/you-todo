c86139b78e938412247713773e7e47ddef564cfa
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2024-11-19 14:08:05 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import CalendarView from '@/views/CalendarView.vue'
-import TodoView from '@/views/TodoView.vue'
-import TimerView from '@/views/TimerView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import CalendarView from '../views/CalendarView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      redirect:'/calendar',
-      meta:{
-        title:'有待办 | 日历'
-      }
-    },
-    {
-      path: '/calendar',
-      name: 'calendar',
-      component: CalendarView,
-      meta:{
-        title:'有待办 | 日历'
-      }
-    },
-    {
-      path: '/todo',
-      name: 'todo',
-      component: TodoView,
-      meta:{
-        title:'有待办 | 待办'
-      }
-    },
-
-    {
-      path: '/timer',
-      name: 'timer',
-      component: TimerView,
-      meta:{
-        title:'有待办 | 专注'
-      }
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL), routes: [{
+        path: '/', redirect: '/calendar', meta: {
+            title: '有待办 | 日历'
+        }
+    }, {
+        path: '/calendar', name: 'calendar', component: CalendarView, meta: {
+            title: '有待办 | 日历'
+        }
+    }, {
+        path: '/todo', name: 'todo', component: () => import('../views/TodoView.vue'), meta: {
+            title: '有待办 | 待办'
+        }
+    }, {
+        path: '/timer', name: 'timer', component: () => import('../views/TimerView.vue'), meta: {
+            title: '有待办 | 专注'
+        }
+    },],
 })
 
 export default router
